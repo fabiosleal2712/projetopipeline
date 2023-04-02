a477262637879a4d1d54757239647df895b07453
fabiosleal212 <lymaleal@gmail.com>
2023-04-02 02:51:35 -0300

    deploy em Terraform

--- vm.tf
+++ vm.tf
@@ -2,5 +2,5 @@ resource "aws_instance" "example" {
   ami           = "ami-007855ac798b5175e"
   instance_type = "t2.micro"
   key_name      = "key_name"
-  public_key    = file("keyterraforn.pub")
+  public_key    = file("./keyterraforn.pub")
 }
