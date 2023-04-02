8c8871946dfb91d2765e32d5691d872650db3fdc
fabiosleal212 <lymaleal@gmail.com>
2023-04-02 19:25:37 -0300

    Deploy em Terraform

--- sg.tf
+++ sg.tf
@@ -1,8 +1,23 @@
 resource "aws_security_group" "sg1" {
-  name        = "example"
+  name        = "sg1"
   description = "Allow SSH inbound traffic"
-  vpc_id      = aws_vpc.vm1.id, aws_vpc.vm2.id
+  vpc_id      = aws_vpc.vm1.id
   
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+
+resource "aws_security_group" "sg2" {
+  name        = "sg2"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.vm2.id
   
 
   ingress {
