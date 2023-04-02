92d86eed9c348402d67ce8ef90489884f5f023ae
fabiosleal212 <lymaleal@gmail.com>
2023-04-02 02:19:18 -0300

    deploy em Terraform

--- main.tf
+++ main.tf
@@ -1,22 +1,3 @@
-terraform {
-
-  required_version = ">= 1.0.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.73.0"
-    }
-  }
-
 provider "aws" {
   region = "us-east-1"
-
-  default_tags {
-    tags = {
-      owner      = "fabioleal"
-      managed-by = "terraform"
-    }
-  }
 }
-
