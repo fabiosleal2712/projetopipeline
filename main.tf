684ed07fb3682dcef6f182c5333c0a06c6384796
fabiosleal212 <lymaleal@gmail.com>
2023-04-02 01:29:12 -0300

    deploy em Terraform

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
     bucket = "fabio-remote-state"
-    key    = "aws-vpc/terraform.tfstate"
+    key    = "aws-vpc54654846/terraform.tfstate"
     region = "us-east-1"
   }
 }
