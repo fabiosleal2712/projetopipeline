2abf04cf34b4c0987dd426f2e668c2f25b875043
fabiosleal212 <lymaleal@gmail.com>
2023-04-02 03:18:30 -0300

    deploy em Terraform

--- vm.tf
+++ vm.tf
@@ -6,7 +6,7 @@ resource "aws_key_pair" "mykey" {
 resource "aws_instance" "example" {
   ami                    = "ami-007855ac798b5175e"
   instance_type          = "t2.micro"
-  key_name               = aws_key_pair.mykey.key_name
+  key_name               = aws_key_pair.mykey.keyterraforn
   vpc_security_group_ids = ["sg-01ceaee86002c8271"]
   subnet_id              = "subnet-0a5dc38d5476fa9b5"
 
