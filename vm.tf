7d41e078d039e031dff79686c89a87fbc0789417
fabiosleal212 <lymaleal@gmail.com>
2023-04-02 19:09:16 -0300

    Deploy em Terraform

--- vm.tf
+++ vm.tf
@@ -10,7 +10,21 @@ resource "aws_instance" "example" {
   vpc_security_group_ids      = ["sg-01ceaee86002c8271"]
   subnet_id                   = "subnet-0a5dc38d5476fa9b5"
   associate_public_ip_address = true
-  count                       = 2
+  
 
   # ...
 }
+
+
+resource "aws_instance" "example2" {
+  ami                         = "ami-007855ac798b5175e"
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.mykey.key_name
+  vpc_security_group_ids      = ["sg-01ceaee86002c8271"]
+  subnet_id                   = "subnet-0a5dc38d5476fa9b5"
+  associate_public_ip_address = true
+  
+
+  # ...
+}
+
