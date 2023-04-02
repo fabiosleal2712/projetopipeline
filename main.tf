30d71ccd62c8318a94501d4e7f0e504c4634abf7
fabiosleal212 <lymaleal@gmail.com>
2023-04-02 01:23:37 -0300

    deploy em Terraform

--- main.tf
+++ main.tf
@@ -1,64 +1,37 @@
+terraform {
+
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.73.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "fabio-remote-state"
+    key    = "aws-vm/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
-}
 
-resource "aws_vpc" "vpc_pcbd" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "vpc_pcbd"
+  default_tags {
+    tags = {
+      owner      = "fabioleal"
+      managed-by = "terraform"
+    }
   }
 }
 
-resource "aws_subnet" "public_subnet_1" {
-  vpc_id     = aws_vpc.vpc_pcbd.id
-  cidr_block = "10.0.1.0/24"
-  tags = {
-    Name = "public_subnet_1"
-  }
-}
-
-resource "aws_subnet" "public_subnet_2" {
-  vpc_id     = aws_vpc.vpc_pcbd.id
-  cidr_block = "10.0.2.0/24"
-  tags = {
-    Name = "public_subnet_2"
-  }
-}
-
-resource "aws_subnet" "public_subnet_3" {
-  vpc_id     = aws_vpc.vpc_pcbd.id
-  cidr_block = "10.0.3.0/24"
-  tags = {
-    Name = "public_subnet_3"
-  }
-}
-
-resource "aws_security_group" "instance_sg" {
-  name_prefix = "instance_sg_"
-  vpc_id      = aws_vpc.vpc_pcbd.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_key_pair" "key_pair" {
-  public_key = file("~/.ssh/id_rsa.pub")
-}
-
-resource "aws_instance" "ec2_instance" {
-  ami           = "ami-0557a15b87f6559cf"
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.key_pair.key_name
-  subnet_id     = "${aws_subnet.public_subnet_1.id}"
-  vpc_security_group_ids = [
-    "${aws_security_group.instance_sg.id}",
-    ]
-  
-  tags = {
-    Name = "pc"
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "fabio-remote-state"
+    key    = "aws-vpc/terraform.tfstate"
+    region = "us-east-1"
   }
 }
