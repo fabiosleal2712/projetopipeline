6610d2b13abc24038c68804923bfa46430859341
fabiosleal212 <lymaleal@gmail.com>
2023-04-02 19:23:26 -0300

    Deploy em Terraform

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "public_ip" {
   value = aws_instance.vm1.public_ip
 }
 
-output "public_ip" {
+output "public_ip1" {
   value = aws_instance.vm2.public_ip
 }
--- sg.tf
+++ sg.tf
@@ -1,8 +1,8 @@
 resource "aws_security_group" "sg1" {
   name        = "example"
   description = "Allow SSH inbound traffic"
-  vpc_id      = aws_vpc.vm1.id
-  vpc_id      = aws_vpc.vm2.id
+  vpc_id      = aws_vpc.vm1.id, aws_vpc.vm2.id
+  
   
 
   ingress {
